7c3ae17477908af7851e222cd5bdd6ad07211da9
hubert <kuchhubert@gmail.com>
2022-08-31 11:14:52 +0200

    Change code from 400 to 404 on not found exception

--- Exceptions/NewsNotFoundException.php
+++ Exceptions/NewsNotFoundException.php
@@ -5,6 +5,6 @@ namespace Hubert\BikeBlog\Exceptions;
 use Avocado\HTTP\HTTPStatus;
 use Avocado\AvocadoApplication\Attributes\Exceptions\ResponseStatus;
 
-#[ResponseStatus(HTTPStatus::BAD_REQUEST)]
+#[ResponseStatus(HTTPStatus::NOT_FOUND)]
 class NewsNotFoundException extends NotFoundException {
 }
