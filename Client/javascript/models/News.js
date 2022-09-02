52215512b3da2cb63519a084902843af5202ffa2
hubert <kuchhubert@gmail.com>
2022-09-02 07:45:04 +0200

    Base model for news

---
+++ Client/javascript/models/News.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class News {
+
+    constructor(params) {
+        this.id = params.id;
+        this.title = params.description;
+        this.tags = params.tags;
+        this.date = params.time;
+    }
+}
