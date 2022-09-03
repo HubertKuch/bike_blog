607e2e2173fdbbfb9df9ed8e33651cf54797dd58
hubert <kuchhubert@gmail.com>
2022-09-03 18:04:46 +0200

    Add meter model

---
+++ Client/javascript/models/Meter.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Meter {
+
+    constructor(data) {
+        this.id = data.id;
+        this.maxSpeed = data.maxSpeed;
+        this.time = data.time;
+        this.newsId = data.newsId;
+    }
+}
