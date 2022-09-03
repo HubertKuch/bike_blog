bde145258c5547ad7fc9af9c0159879bbf6f0136
hubert <kuchhubert@gmail.com>
2022-09-03 21:30:56 +0200

    Make `from` method static

--- Models/DTO/UserDTO.php
+++ Models/DTO/UserDTO.php
@@ -9,7 +9,7 @@ class UserDTO {
     public function __construct(public string $id, public string $username, public string $email, public string $ip, public string $role) {
     }
 
-    public function from(User $user): UserDTO {
+    public static function from(User $user): UserDTO {
         return new UserDTO($user->getId(), $user->getUsername(), $user->getEmail(), $user->getIp()->getIp(), $user->getRole()->value);
     }
 }
