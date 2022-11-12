71ca38c41d1520424d66adc6f284ca1d87711487
hubert <kuchhubert@gmail.com>
2022-11-12 08:57:50 +0100

    Written tags importer from old database

--- bin/importTags.php
+++ bin/importTags.php
@@ -2,7 +2,7 @@
 
 $db = new PDO("mysql:host=127.0.0.1;dbname=bike_blog;port=3306", "root", "");
 
-$stmt = $db->query("SELECT id, tagi FROM bike_blog.newsy");
+$stmt = $db->query("SELECT id, tagi, tytul FROM bike_blog.newsy");
 $stmt->execute();
 
 $data = $stmt->fetchAll(PDO::FETCH_CLASS);
@@ -10,29 +10,37 @@ $tags = array_map(fn($news) => explode(";", $news->tagi), $data);
 $tags = array_merge_recursive(...$tags);
 $tags = array_unique($tags);
 
-//foreach ($tags as $key => $tag) {
-//    $db->query("INSERT INTO bike_blog.tags values(uuid(), '$tag', 'unnamed_descriptor', (SELECT id FROM bike_blog.tags_categories LIMIT 1))");
-//}
+foreach ($tags as $key => $tag) {
+    $db->query("INSERT INTO bike_blog.tags values(uuid(), '$tag', 'unnamed_descriptor', (SELECT id FROM bike_blog.tags_categories LIMIT 1))");
+}
 
 $tagsFromDbStmt = $db->prepare("SELECT id, tag FROM bike_blog.tags");
 $tagsFromDbStmt->execute();
 $tagsFromDb = $tagsFromDbStmt->fetchAll(PDO::FETCH_CLASS);
 
+foreach ($tagsFromDb as $tag) {
+    foreach ($data as $news) {
+        if(str_contains($news->tagi, $tag->tag)) {
+            try {
+                $newsIdStmt = $db->prepare("SELECT news.id FROM bike_blog.news WHERE bike_blog.news.title LIKE '{$news->tytul}'");
+                $newsIdStmt->execute();
+                $newsId = $newsIdStmt->fetchAll(PDO::FETCH_CLASS);
 
-foreach ($data as $oldNew) {
-    foreach ($tagsFromDb as $tag) {
-        if(strlen($tag->tag) == 0 || strlen($oldNew->tagi) == 0) {
-            continue;
-        }
+                if(key($newsId) == null && key($newsId) != 0) {
+                    var_dump(key($newsId));
+                    continue;
+                }
 
-        if(str_contains($oldNew->tagi, $tag->tag)) {
-            $newsIdStmt = $db->prepare("SELECT news.id FROM bike_blog.news WHERE news.title IN (SELECT title FROM bike_blog.newsy WHERE bike_blog.newsy.id = $oldNew->id)");
-            $newsIdStmt->execute();
+                $newsId = $newsId[key($newsId)]->id;
 
-            $newsId = $newsIdStmt->fetchAll(PDO::FETCH_CLASS);
+                if(!$newsId) {
+                    continue;
+                }
 
-            $newsTagStmt = $db->prepare("INSERT INTO bike_blog.news_tag VALUE (uuid(), '{$newsId[key($newsId)]->id}', '{$tag->id}')");
-            $newsTagStmt->execute();
+                $newsTagStmt = $db->prepare("INSERT INTO bike_blog.news_tag VALUE (uuid(), '{$newsId}', '{$tag->id}')");
+                $newsTagStmt->execute();
+            } catch (Error) {
+            }
         }
     }
 }
