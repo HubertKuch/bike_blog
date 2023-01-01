fb0a8989b5654d7e13b1e0d907bd3340edae3e8b
hubert <kuchhubert@gmail.com>
2023-01-01 18:10:28 +0100

    Parsing meters tag

--- Utils/NewsHTMLParser.php
+++ Utils/NewsHTMLParser.php
@@ -2,9 +2,11 @@
 
 namespace Hubert\BikeBlog\Utils;
 
+use Avocado\ORM\AvocadoRepository;
 use AvocadoApplication\Attributes\Autowired;
 use AvocadoApplication\Attributes\Resource;
 use Hubert\BikeBlog\Configuration\ImagesConfiguration;
+use Hubert\BikeBlog\Models\Meter\Meter;
 use Hubert\BikeBlog\Models\News\News;
 use Hubert\BikeBlog\Services\ImagesService;
 
@@ -17,6 +19,8 @@ class NewsHTMLParser {
     private ImagesService $imagesService;
     #[Autowired]
     private ImagesConfiguration $imagesConfiguration;
+    #[Autowired("metersRepository")]
+    private AvocadoRepository $metersRepository;
 
     public function __construct() {
     }
@@ -25,6 +29,7 @@ class NewsHTMLParser {
         $newsBody = $news->getDescription();
 
         $newsBody = self::parseImages($newsBody, $news);
+        $newsBody = self::parseMeters($newsBody, $news);
 
         $news->setDescription($newsBody);
 
@@ -46,5 +51,36 @@ class NewsHTMLParser {
         }
 
         return $this->customHTMLTagsParser->parseTag(new HTMLTag("zdjecia"), $content, $opening . "</div>");
+    }
+
+    private function parseMeters(string $content, News $news): string {
+        $opening = "<div class='meters news-meters'>";
+        $parsedContent = "";
+        $closing = "</div>";
+
+        /** @var Meter[] $meters */
+        $meters = $this->metersRepository->findMany(['news_id' => $news->getId()]);
+
+        for ($index = 0; $index < count($meters); $index++) {
+            $meter = $meters[$index];
+            $meterNo = $index + 1;
+
+            $parsedContent .= <<<EOL
+                    <div class="meter">
+                        <h2>Licznik nr {$meterNo}</h2>
+                        <ul>
+                            <li>Poczatkowy stan {$meter->getStartState()}</li>                                      
+                            <li>Koncowy stan {$meter->getEndState()}</li>                                      
+                            <li>Maksymalna predkosc {$meter->getMaxSpeed()}</li>                                      
+                            <li>Czas na liczniku {$meter->getTime()}</li>                                      
+                            <li>Czas jazdy {$meter->getTripLength()}</li>                                      
+                        </ul>    
+                    </div>      
+            EOL;
+        }
+
+        $parsedContent = $opening . $parsedContent . $closing;
+
+        return $this->customHTMLTagsParser->parseTag(new HTMLTag("statystyka"), $content, $parsedContent);
     }
 }
